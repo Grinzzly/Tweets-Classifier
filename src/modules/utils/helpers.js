cbd291671087c7cf8dd815efb97a9484defb4aea
Sergey Grin <grinzzly@gmail.com>
2019-11-27 01:32:52 +0100

    Peovide possible author counter

--- src/modules/utils/helpers.js
+++ src/modules/utils/helpers.js
@@ -3,7 +3,7 @@ import { trainingData } from './training-data';
 
 let trainedNet;
 
-const encoder = (arg) => (arg.split('').map((x) => (x.charCodeAt(0) / 256)));
+const encoder = (arg) => ({...arg.split('').map((x) => (x.charCodeAt(0) / 256))});
 
 const processTrainingData = (data) => {
   return data.map((d) => {
@@ -16,25 +16,32 @@ const processTrainingData = (data) => {
 
 const train = (data) => {
   const net = new NeuralNetwork();
+
   net.train(processTrainingData(data));
-  console.log(net);
   trainedNet = net.toFunction();
 };
 
 export const execute = (input) => {
   const results = trainedNet(encoder(input));
+  const possibleAuthor = Object.keys(results).reduce((a, b) => (results[a] > results[b] ? a : b));
   let output;
   let certainty;
 
-  if (results.trump > results.kardashian) {
-    output = 'Donald Trump';
-    certainty = Math.floor(results.trump * 100);
-  } else {
-    output = 'Kim Kardashian';
-    certainty = Math.floor(results.kardashian * 100);
+  switch (possibleAuthor) {
+    case 'trump':
+      output = 'Donald Trump';
+      certainty = Math.floor(results.trump * 100);
+      break;
+    case 'kardashian':
+      output = 'Kim Kardashian';
+      certainty = Math.floor(results.kardashian * 100);
+      break;
+    default:
+      output = 'Grinzzly';
+      certainty = Math.floor(results.grinzzly * 100);
   }
 
-  return `I'm ${certainty} % sure that tweet was written by ${output}`;
+  return `I'm ${certainty} % sure that such tweet may be written by ${output}`;
 };
 
 train(trainingData);
